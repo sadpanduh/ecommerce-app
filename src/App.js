e591518f410e8a5bbc3673b9340a11ffd4de0c49
Anthony <noneedforemail>
2019-08-22 12:51:29 -0700

    finish sign in handle submit method

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends React.Component{
             }
           })
         });
-        
       }else{
         //if its null just set the current user to null, useful when you logout
         this.setState({currentUser: userAuth});
