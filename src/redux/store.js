a15d0d41c1748790133cdb06899d7584408bb1a0
Anthony <noneedforemail>
2019-09-11 10:51:47 -0700

    make redux-logger only work for developmet environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,13 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+//only include logger for development environment 
+//has three values, development/production/test
+if(process.env.NODE_ENV === 'development'){
+    middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
