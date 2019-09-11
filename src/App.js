b505e2cde4d51ce0707f4ce9504379183deead4c
Anthony <noneedforemail>
2019-09-11 16:38:46 -0700

    add contact page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {createStructuredSelector} from 'reselect';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+import ContactPage from './pages/contact/contact-page.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
@@ -53,6 +54,7 @@ class App extends React.Component{
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/contact' component={ContactPage} />
           <Route exact path='/signin'
            render={() => this.props.currentUser ? (<Redirect to='/' />) : <SignInAndSignUpPage />} 
           />
---
+++ src/pages/contact/contact-page.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './contact-page.styles.scss';
+
+const ContactPage = () => (
+    <div className='contact-page'>
+        <div className='social'>
+            <a href="https://www.linkedin.com/in/soanthony">
+                <img className='linked-in'
+                src='https://upload.wikimedia.org/wikipedia/commons/0/01/LinkedIn_Logo.svg' 
+                alt='linkedin'/>
+            </a>
+        </div>
+    </div>
+);
+
+export default ContactPage;
